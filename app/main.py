776c9c821aa95d2663f785c5d78d29ed087f603d
sangyeop12 <kssy5554@gmail.com>
2026-04-07 20:18:05 +0900

    add external router

--- app/main.py
+++ app/main.py
@@ -8,5 +8,8 @@ app.include_router(prescription.router)
 app.include_router(prescription_drugs.router)
 app.include_router(schedules.router)
 app.include_router(medication_logs.router)
-
 app.include_router(external.router)
+
+@app.get("/")
+def root():
+    return {"message": "알콩약콩 서버 실행 중"}
